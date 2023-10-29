34b381bf80204e830524d48c2b37c5f2db89fc10
Umigishi-Aoi <umigishi.aoi@gmail.com>
2023-10-29 15:46:15 +0900

    delete counter app and initialize app

---
+++ lib/flutter_tetris.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FlutterTetris extends StatelessWidget {
+  const FlutterTetris({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
