bf7595528efd208e4c407ad1f4fd929d785cc8e1
Umigishi-Aoi <umigishi.aoi@gmail.com>
2024-01-21 11:13:55 +0900

    add keep function

---
+++ lib/tetris/keep_mino/keep_mino.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tetris/tetris/config/mino_config.dart';
+import 'package:flutter_tetris/tetris/mino/mino.dart';
+
+import '../config/configs.dart';
+
+class KeepMino extends StatelessWidget {
+  const KeepMino({required this.config, super.key});
+
+  final MinoConfig? config;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: panelSize * notShowMinoVerticalNumber,
+          width: panelSize * 4,
+        ),
+        if (config != null) Mino(minoPanel: config!.nextMino()),
+      ],
+    );
+  }
+}
